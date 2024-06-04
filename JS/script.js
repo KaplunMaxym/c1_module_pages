ad043b61a76d84907446d15bfee9c7cc0b8a5a37
KaplunMaxym <61882640+KaplunMaxym@users.noreply.github.com>
2024-06-04 16:44:59 +0300

    copy to clipboard

--- JS/script.js
+++ JS/script.js
@@ -114,28 +114,21 @@ window.addEventListener('load', ()=>{
 
     // custom select script
     let x, i, j, l, ll, selElmnt, a, b, c;
-    /* Look for any elements with the class "custom-select": */
     x = document.getElementsByClassName("custom-select");
     l = x.length;
     for (i = 0; i < l; i++) {
     selElmnt = x[i].getElementsByTagName("select")[0];
     ll = selElmnt.length;
-    /* For each element, create a new DIV that will act as the selected item: */
     a = document.createElement("DIV");
     a.setAttribute("class", "select-selected");
     a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
     x[i].appendChild(a);
-    /* For each element, create a new DIV that will contain the option list: */
     b = document.createElement("DIV");
     b.setAttribute("class", "select-items select-hide");
     for (j = 1; j < ll; j++) {
-        /* For each option in the original select element,
-        create a new DIV that will act as an option item: */
         c = document.createElement("DIV");
         c.innerHTML = selElmnt.options[j].innerHTML;
         c.addEventListener("click", function(e) {
-            /* When an item is clicked, update the original select box,
-            and the selected item: */
             let y, i, k, s, h, sl, yl;
             s = this.parentNode.parentNode.getElementsByTagName("select")[0];
             sl = s.length;
@@ -159,18 +152,13 @@ window.addEventListener('load', ()=>{
     }
     x[i].appendChild(b);
     a.addEventListener("click", function(e) {
-        /* When the select box is clicked, close any other select boxes,
-        and open/close the current select box: */
         e.stopPropagation();
         closeAllSelect(this);
         this.nextSibling.classList.toggle("select-hide");
         this.classList.toggle("select-arrow-active");
     });
     }
-
     function closeAllSelect(elmnt) {
-    /* A function that will close all select boxes in the document,
-    except the current select box: */
     let x, y, i, xl, yl, arrNo = [];
     x = document.getElementsByClassName("select-items");
     y = document.getElementsByClassName("select-selected");
@@ -189,9 +177,22 @@ window.addEventListener('load', ()=>{
         }
     }
     }
-
-    /* If the user clicks anywhere outside the select box,
-    then close all select boxes: */
     document.addEventListener("click", closeAllSelect);
+
+    // copy to clipbouar
+    document.querySelectorAll('.keyContainer').forEach(container => {
+        container.querySelector('[data-copy-type="key"]').addEventListener('click', () => {
+            const key = container.querySelector('.key_area').dataset.copyData;
+            navigator.clipboard.writeText(key).then(() => {
+                let oldDaata = container.querySelector('[data-copy-type="key"]').innerHTML;
+                container.querySelector('[data-copy-type="key"]').innerHTML = "Скопійовано !";
+                setTimeout(()=>{
+                    container.querySelector('[data-copy-type="key"]').innerHTML = oldDaata;
+                }, 800)
+            }).catch(err => {
+                console.error('Failed to copy key: ', err);
+            });
+        });
+    });
     
 })
